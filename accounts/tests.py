dd61f365beb976e7e461c68c0629d666a9040ef4
Marten <marten.knbk@gmail.com>
2018-03-19 13:15:03 +0100

    Added migrations and first test.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,9 @@
+from django.urls import reverse
+
 from rest_framework.test import APITestCase
 
 
-class AccountsTestCase(TestCase):
+class AccountsTestCase(APITestCase):
     def test_list_accounts(self):
         response = self.client.get(reverse('student-list'))
         self.assertEqual(response.status_code, 200)
