56409879b91b882cc58587c0ce315cf4b97b1f28
Marten <marten.knbk@gmail.com>
2018-03-19 23:38:41 +0100

    Changed root router to SimpleRouter, added tutor locations router.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_nested import routers
 from .views import StudentViewSet, UserViewSet, TutorViewSet, LocationViewSet
 
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'students', StudentViewSet)
 router.register(r'tutors', TutorViewSet)
 router.register(r'users', UserViewSet)
@@ -15,7 +15,4 @@ student_location_router.register(r'locations', LocationViewSet, base_name='stude
 tutor_location_router = routers.NestedDefaultRouter(router, r'tutors', lookup='tutor')
 tutor_location_router.register(r'locations', LocationViewSet, base_name='tutor-locations')
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(student_location_router.urls)),
-]
+urlpatterns = router.urls + student_location_router.urls + tutor_location_router.urls
