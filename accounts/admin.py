8a25e318a7c9bebe6632b87245304ae184f9ff48
Marten <marten.knbk@gmail.com>
2018-03-19 14:19:47 +0100

    Added models to admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Profile, Tutor, Student, Location
+
+
+admin.site.register([Profile, Tutor, Student, Location])
+admin.site.register(User, UserAdmin)
