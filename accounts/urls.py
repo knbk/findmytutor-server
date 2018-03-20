1f7eab6538b11e19d7abd334dc5dc3a70a836fc9
Marten <marten.knbk@gmail.com>
2018-03-20 23:59:22 +0100

    Updated urls structure.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
+from meetings.views import MeetingViewSet
 from .views import StudentViewSet, UserViewSet, TutorViewSet, LocationViewSet
 
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register(r'students', StudentViewSet)
 router.register(r'tutors', TutorViewSet)
 router.register(r'users', UserViewSet)
+router.register(r'meetings', MeetingViewSet, base_name='meeting')
 
 student_location_router = routers.NestedDefaultRouter(router, r'students', lookup='student')
 student_location_router.register(r'locations', LocationViewSet, base_name='student-locations')
@@ -16,6 +18,3 @@ tutor_location_router = routers.NestedDefaultRouter(router, r'tutors', lookup='t
 tutor_location_router.register(r'locations', LocationViewSet, base_name='tutor-locations')
 
 urlpatterns = router.urls + student_location_router.urls + tutor_location_router.urls
-urlpatterns += [
-    path('', include('meetings.urls')),
-]
