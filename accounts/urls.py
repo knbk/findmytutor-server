e2344e60f0305888983e1aa490cc9a08402cd3e8
Marten <marten.knbk@gmail.com>
2018-03-26 14:41:03 +0200

    Updated url config.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework_nested import routers
 
 from meetings.views import MeetingViewSet
+from messages.views import MessageThreadViewSet
 from .views import StudentViewSet, UserViewSet, TutorViewSet, LocationViewSet
 
 
@@ -10,6 +11,7 @@ router.register(r'students', StudentViewSet)
 router.register(r'tutors', TutorViewSet)
 router.register(r'users', UserViewSet)
 router.register(r'meetings', MeetingViewSet, base_name='meeting')
+router.register(r'messages', MessageThreadViewSet, base_name='messagethread')
 
 student_location_router = routers.NestedDefaultRouter(router, r'students', lookup='student')
 student_location_router.register(r'locations', LocationViewSet, base_name='student-locations')
@@ -18,6 +20,3 @@ tutor_location_router = routers.NestedDefaultRouter(router, r'tutors', lookup='t
 tutor_location_router.register(r'locations', LocationViewSet, base_name='tutor-locations')
 
 urlpatterns = router.urls + student_location_router.urls + tutor_location_router.urls
-urlpatterns += [
-    path('', include('messages.urls')),
-]
