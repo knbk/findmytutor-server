5c05032c640fc22f0e76c72d731e89e8c3f3b16a
Marten <marten.knbk@gmail.com>
2018-04-04 09:20:20 +0200

    Fixed import.

--- findmytutor/urls.py
+++ findmytutor/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts.urls import get_token
+from accounts.views import get_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
