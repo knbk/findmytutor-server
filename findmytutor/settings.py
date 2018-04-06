6733b0aacda78b6bfbf25aeea1aa4dca330b8c82
Marten <marten.knbk@gmail.com>
2018-04-06 13:51:14 +0200

    Added contrib.gis to installed apps.

--- findmytutor/settings.py
+++ findmytutor/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 
     'rest_framework',
     'rest_framework.authtoken',
