c35f337f4f598e9f6dac9d9dedc0d9ba20d97562
Marten <marten.knbk@gmail.com>
2018-04-13 09:43:51 +0200

    Added full location to meeting serializer.

--- meetings/serializers.py
+++ meetings/serializers.py
@@ -1,5 +1,10 @@
+
+from django.db.transaction import atomic
 from rest_framework import serializers
 
+
+from accounts.serializers import LocationSerializer
+from accounts.models import Location
 from .models import Meeting, Review
 
 
@@ -13,6 +18,7 @@ class MeetingSerializer(serializers.ModelSerializer):
     is_accepted = serializers.BooleanField(read_only=True)
     is_cancelled = serializers.BooleanField(read_only=True)
     review = ReviewSerializer(read_only=True)
+    location = LocationSerializer()
 
     def validate(self, attrs):
         if attrs['start'] >= attrs['end']:
@@ -22,3 +28,14 @@ class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = ['pk', 'tutor', 'student', 'start', 'end', 'location', 'is_accepted', 'is_cancelled', 'review']
+
+    @atomic()
+    def create(self, validated_data):
+        validated_data['location'] = Location.objects.create(**validated_data.pop('location'))
+        return super().perform_create(validated_data)
+
+    @atomic()
+    def update(self, instance, validated_data):
+        location = validated_data.pop('location')
+        LocationSerializer(instance.location, data=location).save()
+        return super().update(instance, validated_data)
