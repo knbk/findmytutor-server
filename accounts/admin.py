1089895cb1bba349d15a70c2fb9dd20fd0841e79
Marten <marten.knbk@gmail.com>
2018-04-14 18:00:48 +0200

    Added ProfilePicture to admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Tutor, Student, Location
+from .models import User, Tutor, Student, Location, ProfilePicture
 
 
-admin.site.register([Tutor, Student, Location])
+admin.site.register([Tutor, Student, Location, ProfilePicture])
 admin.site.register(User, UserAdmin)
